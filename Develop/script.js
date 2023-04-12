cf26eabfffed181c9ce1083e99269f5ea287e2bc
kmfox1087 <kmfox1087@gmail.com>
2023-04-12 18:15:33 -0400

    added event listener and password generation function

--- Develop/script.js
+++ Develop/script.js
@@ -10,7 +10,22 @@ function writePassword() {
   passwordText.value = password;
 
 }
+
 // Password variable values
+
 character = ["!", "@", "#", "$", "%", "^", "&", "(", ")", "-", "_", "=", "+", "~", "`", ";", ":", ">", "<", "/", "?", "'", "[", "]", "{", "}"]
+number = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+get.addEventListener("click", function () {
+  newps = generatePassword();
+  document.getElementById("password").placeholder = newps;
+});
+
+// Generate password
+function generatePasword() {
+  enter = parseInt(prompt("How many characters would you like yout password to be? Choose between 8 and 128"))
+}
